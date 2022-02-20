dd9e11d0676874eddf3b4fe8d5005e9dd7a09552
nblock <natep15@outlook.com>
2022-02-20 18:39:59 -0500

    textures export properly (extra fast now that it doesnt write a temp file)

--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "helpers.h"
 #include "fbxmodel.h"
+#include "texture.h"
 #include <sarge.cpp>
 
 unsigned char* data = nullptr;
@@ -8,11 +9,6 @@ std::string hash = "";
 std::string pkgID = "";
 std::string packagesPath = "";
 bool lodCulling = true;
-int textureFormat;
-uint16_t width;
-uint16_t height;
-uint16_t arraySize;
-std::string largeHash;
 Submesh* submesh = new Submesh();
 FbxModel* fbxModel = new FbxModel();
 int stride;
