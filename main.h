2f2b8db3362420e8cba322c1b3e6085595438629
nblock <34419030+nblockbuster@users.noreply.github.com>
2022-02-20 16:39:24 -0500

    Update main.h

--- main.h
+++ main.h
@@ -4,56 +4,18 @@
 #include <sarge.cpp>
 
 unsigned char* data = nullptr;
-unsigned char* lzdata = nullptr;
 std::string hash = "";
 std::string pkgID = "";
 std::string packagesPath = "";
 bool lodCulling = true;
-//float lzscale;
-
-struct Vector3
-{
-	float x;
-	float y;
-	float z;
-};
-
-struct Vector4
-{
-	float x;
-	float y;
-	float z;
-	float w;
-};
-
-struct Vector2
-{
-	float u;
-	float v;
-};
-
-struct Faces
-{
-	int16_t x;
-	int16_t y;
-	int16_t z;
-};
-
-std::vector<Vector4> Translation;
-std::vector<uint16_t> PosLookup;
-
 int textureFormat;
 uint16_t width;
 uint16_t height;
 uint16_t arraySize;
 std::string largeHash;
-
 Submesh* submesh = new Submesh();
 FbxModel* fbxModel = new FbxModel();
 int stride;
-
 int getFile();
-
 std::vector<FbxNode*> nodes;
-//std::vector<StaticMesh*> meshes;
 void addVertColSlots(Submesh* submesh);
